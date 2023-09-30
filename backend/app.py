54f1ab69799e3b5e4e2665ad6a0152572b299122
shehabmohsen <aquashehab612@gmaiil.com>
2023-09-29 22:22:50 -0400

    feat: add cart routes to app

--- backend/app.py
+++ backend/app.py
@@ -1,8 +1,26 @@
 from flask import Flask
+from cart import getCart
+from flask import request
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
 
+@app.route("/cart", methods = ["GET"])
+def getCartRoute():
+    data = request.get_json()
+    return getCart(data)
 
+@app.route("/cart/add", methods =["PUT"])
+def addToCart():
+    pass
+
+@app.route("/cart/subtract", methods=["PUT"])
+def subtractFromCart():
+    pass
+
+@app.route("/cart/delete", methods=["DELETE"])
+def deleteFromCart(): 
+    pass
