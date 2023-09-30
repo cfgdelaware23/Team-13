3327fc60b743dff634c761fe8d0e6843740dab0b
Josephine Lee <lee.josie.y@gmail.com>
2023-09-30 10:34:07 -0400

    add doc strings for backend functions

--- backend/db.py
+++ backend/db.py
@@ -10,6 +10,13 @@ c = db.cursor()               #facilitate db ops -- you will use cursor to trigg
 
 
 def create_tables():
+    '''
+    Initializes SQL tables.
+    users: table of users
+    cart: each user has a cart with overall statistics
+    cartcontent: the content of each user's cart 
+    products: all the products available in the store
+    '''
     c = db.cursor()
     c.execute("CREATE TABLE IF NOT EXISTS users(id TEXT, modifier REAL)")
     c.execute("CREATE TABLE IF NOT EXISTS cart(user_id INTEGER, total_price TEXT, money_saved TEXT)")
@@ -18,12 +25,18 @@ def create_tables():
     db.commit() 
 
 def populate_tables():
+    '''
+    Load product data into products table
+    '''
     prod_data_csv = open('prodData.csv', encoding='utf-8-sig')
     prod_content = csv.reader(prod_data_csv)
     insert_prod_records = "INSERT INTO products (id, price, name, sku, category, image_url, aisle) VALUES (?, ?, ?, ?, ?, ?, ?)"
     c.executemany(insert_prod_records, prod_content)
 
 def create_user(id, modifier):
+    '''
+    Enter new user into the system
+    '''
     c = db.cursor()
     create_tables()
     c.execute("INSERT INTO users (id, modifier) VALUES (?, ?);", (id, modifier))
@@ -44,6 +57,9 @@ def get_cart(user_id):
     return cart_metadata, cart_products
 
 def add_product_to_cart(user_id, product_id, quantity) -> bool:
+    '''
+    Adds one of the designated product to a user's cart
+    '''
 
     try: 
         c = db.cursor()
@@ -78,6 +94,9 @@ def add_product_to_cart(user_id, product_id, quantity) -> bool:
 
 
 def remove_product_from_cart(user_id, product_id):
+    '''
+    Removes one of an item from the user's cart
+    '''
     c = db.cursor()
     #check if there are multiple of product
     c.execute("SELECT id, quantity FROM cartcontent WHERE quantity > 1 AND card_id = ? AND product_id = ?;", (user_id, product_id))
@@ -102,6 +121,9 @@ def remove_product_from_cart(user_id, product_id):
     db.commit()
 
 def empty_cart(user_id):
+    '''
+    Completely empties a user's cart
+    '''
     c = db.cursor()
     c.execute("DELETE FROM cartcontent WHERE card_id = ?;", (user_id,))
     c.execute("UPDATE cart SET total_price = 0 WHERE user_id = ?;", (user_id,))
@@ -109,12 +131,18 @@ def empty_cart(user_id):
     db.commit()
 
 def get_product_info(product_id):
+    '''
+    Gets info for a particular product
+    '''
     c = db.cursor()
     c.execute("SELECT * FROM products WHERE id = ?;", (product_id,))
     product_data = c.fetchone()
     return product_data
 
 def calculate_cart_total(user_id):
+    '''
+    Gets the total price in a user's cart
+    '''
     c = db.cursor()
     c.execute("SELECT total_price FROM cart WHERE user_id = ?;", (user_id,))
     #c.execute("SELECT SUM(p.price * cc.quantity) FROM products AS p JOIN cartcontent AS cc ON p.id = cc.product_id WHERE cc.card_id = ?;", (user_id,))
@@ -122,29 +150,45 @@ def calculate_cart_total(user_id):
     return total_price if total_price else 0
 
 def get_cart_contents(user_id):
+    '''
+    Gets the products and quantities in a user's cart
+    '''
     c = db.cursor()
     c.execute("SELECT p.*, cc.quantity FROM products AS p JOIN cartcontent AS cc ON p.id = cc.product_id WHERE cc.card_id = ?;", (user_id,))
     cart_contents = c.fetchall()
     return cart_contents
 
 def get_products_of_type(type):
+    '''
+    Gets all the products available of a certain type (fruit, vegetable, etc)
+    '''
     c = db.cursor()
     c.execute("SELECT id, price, name, sku, image_url, aisle FROM products WHERE category = ?;", (type,))
     type_contents = c.fetchall()
     return type_contents
 
 def get_products():
+    '''
+    Gets all the products from the products table
+    '''
     c = db.cursor()
     c.execute("SELECT * from products")
     products = c.fetchall()
     return products
 
 def add_product(product_id, product_price, product_name, product_sku, prod_category, prod_image, prod_aisle):
+    '''
+    Adds a product to the overall product table
+    '''
     c = db.cursor()
     c.execute("INSERT INTO products (id, price, name, sku, category, image_url, aisle) VALUES (?, ?, ?, ?, ?, ?, ?);", (product_id, product_price, product_name, product_sku, prod_category, prod_image, prod_aisle))
     db.commit()
 
 def get_user_modifier(user_id:float): 
+    '''
+    Gets the modifier (how much the original price is modified) for a 
+    particular user
+    '''
     print(type(user_id))
     c = db.cursor()
     c.execute("SELECT modifier from users where id = ?", (user_id,))
@@ -179,8 +223,8 @@ for user in user_rows:
 for prod in prod_rows:
     print(prod)
 """
-add_product_to_cart("512380", '1', 1)
-add_product_to_cart("512380", '1', 1)
+# add_product_to_cart("512380", '1', 1)
+# add_product_to_cart("512380", '1', 1)
 """
 print(get_cart("512380"))
 remove_product_from_cart("512380", '1')
