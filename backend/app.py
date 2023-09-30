79aae0cc12db2f00880bdf6e1e4f4aaf1655187b
Josephine Lee <lee.josie.y@gmail.com>
2023-09-29 20:09:30 -0400

    create initial databases

--- backend/app.py
+++ backend/app.py
@@ -4,3 +4,17 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
+
+from flask import Flask             #facilitate flask webserving
+from flask import render_template   #facilitate jinja templating
+from flask import request           #facilitate form submission
+from flask import session           #facilitate flask sessions
+import sqlite3   #enable control of an sqlite database
+
+db = sqlite3.connect("chocolate.db", check_same_thread=False) #open if file exists, otherwise create
+c = db.cursor()               #facilitate db ops -- you will use cursor to trigger db events
+
+
+c.execute("CREATE TABLE IF NOT EXISTS users(username TEXT)")
+c.execute("CREATE TABLE IF NOT EXISTS products(name TEXT)")
+db.commit()
