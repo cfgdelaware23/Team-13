c7cda63c9d2deda195504f2ebe6396e7b747ab7f
Josephine Lee <lee.josie.y@gmail.com>
2023-09-29 21:08:40 -0400

    add functions for cart

--- backend/db.py
+++ backend/db.py
@@ -11,14 +11,42 @@ c = db.cursor()               #facilitate db ops -- you will use cursor to trigg
 def create_tables():
     c = db.cursor()
     c.execute("CREATE TABLE IF NOT EXISTS users(id INTEGER, modifier INTEGER)")
-    c.execute("CREATE TABLE IF NOT EXISTS cart(id INTEGER, user_id INTEGER, total_price TEXT, money_saved TEXT)")
-    c.execute("CREATE TABLE IF NOT EXISTS cartcontent(id INTEGER, card_id INTEGER, product_id TEXT, quantity INTEGER)")
-    c.execute("CREATE TABLE IF NOT EXISTS products(id INTEGER, price TEXT, name TEXT, sku TEXT, category TEXT, image_url TEXT)")
+    c.execute("CREATE TABLE IF NOT EXISTS cart(user_id INTEGER, total_price TEXT, money_saved TEXT)")
+    c.execute("CREATE TABLE IF NOT EXISTS cartcontent(card_id INTEGER, product_id TEXT, quantity INTEGER)")
+    c.execute("CREATE TABLE IF NOT EXISTS products(id TEXT, price TEXT, name TEXT, location TEXT, sku TEXT, category TEXT, image_url TEXT)")
     db.commit()
 
 def create_user(id, modifier):
     c = db.cursor()
     create_tables()
-    c.execute(f'INSERT INTO users (id, modifier) VALUES (?, ?);', (id, modifier))
-    c.execute(f'INSERT INTO cart (id, user_id, total_price, money_saved) VALUES (?, ?, ?, ?);', (id, id, 0, 0))
+    c.execute("INSERT INTO users (id, modifier) VALUES (?, ?);", (id, modifier))
+    c.execute("INSERT INTO cart (user_id, total_price, money_saved) VALUES (?, ?, ?);", (id, 0, 0))
+    db.commit()
+
+def get_cart(user_id):
+    c = db.cursor()
+    c.execute("SELECT * FROM cart WHERE user_id = ?;", (user_id,))
+    cart_data = c.fetchone()
+    return cart_data
+
+def add_product_to_cart(user_id, product_id, quantity):
+    c = db.cursor()
+    
+    c.execute("SELECT id, quantity FROM cartcontent WHERE card_id = ? AND product_id = ?;", (user_id, product_id))
+    existing_product = c.fetchone()
+    if existing_product:
+        new_quantity = existing_product[1] + quantity
+        c.execute("UPDATE cartcontent SET quantity = ? WHERE id = ?;", (new_quantity, existing_product[0]))
+    else:
+        c.execute("INSERT INTO cartcontent (card_id, product_id, quantity) VALUES (?, ?, ?);", (user_id, product_id, quantity))
+    db.commit()
+
+def remove_product_from_cart(user_id, product_id):
+    c = db.cursor()
+    c.execute("DELETE FROM cartcontent WHERE card_id = ? AND product_id = ?;", (user_id, product_id))
+    db.commit()
+
+def empty_cart(user_id):
+    c = db.cursor()
+    c.execute("DELETE FROM cartcontent WHERE card_id = ?;", (user_id,))
     db.commit()
