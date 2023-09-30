e9a7004cd6ea3630c2714f3b2c65d76f3f29bd62
Josephine Lee <lee.josie.y@gmail.com>
2023-09-29 20:21:06 -0400

    added attributes to sql tables

--- backend/app.py
+++ backend/app.py
@@ -15,6 +15,8 @@ db = sqlite3.connect("chocolate.db", check_same_thread=False) #open if file exis
 c = db.cursor()               #facilitate db ops -- you will use cursor to trigger db events
 
 
-c.execute("CREATE TABLE IF NOT EXISTS users(username TEXT)")
-c.execute("CREATE TABLE IF NOT EXISTS products(name TEXT)")
+c.execute("CREATE TABLE IF NOT EXISTS users(id INTEGER, issnap TEXT, modifier INTEGER)")
+c.execute("CREATE TABLE IF NOT EXISTS cart(id INTEGER, user_id INTEGER, total_price TEXT, money_saved TEXT)")
+c.execute("CREATE TABLE IF NOT EXISTS cartcontent(id INTEGER, card_id INTEGER, product_id TEXT, quantity INTEGER)")
+c.execute("CREATE TABLE IF NOT EXISTS products(id INTEGER, price TEXT, name TEXT, sku TEXT, category TEXT, image_url TEXT)")
 db.commit()
