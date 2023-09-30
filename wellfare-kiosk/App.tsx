7e50d92977ebcb6a927c8371222ca04dd990d548
arjuntrivedi <arjuntrivedi11@gmail.com>
2023-09-29 20:36:56 -0400

    Added food categories to frontend

--- wellfare-kiosk/App.tsx
+++ wellfare-kiosk/App.tsx
@@ -1,14 +1,31 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, View } from 'react-native';
-import Welcome from './components/welcome'; // Make sure to adjust the path if your file structure is different
+import Welcome from './components/welcome'; 
+import Category from './components/category';
+import Fruits from './components/fruits';
+import Vegetables from './components/vegetables';
+import Protein from './components/protein';
+
+import { createStackNavigator } from '@react-navigation/stack';
+import { NavigationContainer } from '@react-navigation/native';
+
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Welcome />
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Welcome">
+        <Stack.Screen name="Welcome" component={Welcome} options={{ headerShown: false }} />
+        <Stack.Screen name="Category" component={Category} />
+        <Stack.Screen name="Fruits" component={Fruits} />
+        <Stack.Screen name="Vegetables" component={Vegetables} />
+        <Stack.Screen name="Protein" component={Protein} />
+
+        {/* ... Other category screens like Vegetables, Protein, etc. */}
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
