ab3e0ae8c0871adc65eadb964c4cd4a20a55a12a
Josh Feuerstein <jtf82@cornell.edu>
2023-09-29 21:20:34 -0400

    implemented cartItem

--- wellfare-kiosk/App.tsx
+++ wellfare-kiosk/App.tsx
@@ -1,13 +1,27 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import CartIncrement from './components/CartIncrement';
 import { useState } from 'react';
+import CartItem from './components/CartItem';
+
+const ex_product = {
+  price: 15,
+  name: "apple",
+  sku: 12345,
+  category: "fruit",
+  imageURL: "https://thumbs.dreamstime.com/b/red-apple-isolated-clipping-path-19130134.jpg",
+  location: "aisle 1"
+}
+
+const ex = {
+startCount: 1,
+product: ex_product
+}
 
 export default function App() {
   const [count, setCount] = useState(2);
   return (
     <View style={styles.container}>
-      <CartIncrement setCount = {setCount} count = {count}/>
+      <CartItem startCount= {1} product= {ex_product}/>
     </View>
   )
 }
